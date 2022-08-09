aa3168b796d5d87eeb4c119f81ca43c27a239ff0
xiaohaoxing <xiaohaoxing@outlook.com>
2022-08-09 16:14:51 +0800

    feat: 连接时发送实时数据给前端。

--- src/main/java/whu/edu/cs/transitnet/realtime/RealtimeSocketHandler.java
+++ src/main/java/whu/edu/cs/transitnet/realtime/RealtimeSocketHandler.java
@@ -1,6 +1,8 @@
 package whu.edu.cs.transitnet.realtime;
 
+import com.alibaba.fastjson.JSON;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.TextMessage;
@@ -11,11 +13,17 @@ import org.springframework.web.socket.handler.TextWebSocketHandler;
 @Component
 public class RealtimeSocketHandler extends TextWebSocketHandler {
 
+    @Autowired
+    private RealtimeService realtimeService;
+
     @Override
     public void afterConnectionEstablished(WebSocketSession session) throws Exception {
         // TODO: 校验 token 避免 DDOS 攻击
         String token = session.getId();
         WsSocketManager.add(token, session);
+        // 连接后即将实时数据发送给客户端。
+        String msg = JSON.toJSONString(realtimeService.getAllVehicles());
+        session.sendMessage(new TextMessage(msg));
     }
 
     @Override
